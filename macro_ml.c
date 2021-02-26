93f2cb4e92b7ccf9604e635192cefe935732beb7
Sumit Kumar <sumitkn.pine@gmail.com>
2021-02-26 21:54:37 +0530

    C code

---
+++ macro_ml.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define MACRO(num, str) ({\
+            printf("%d", num);\
+            printf(" is");\
+            printf(" %s number....!!!!\n", str);\
+           })
+
+int main(void)
+{
+    int num;
+
+    printf("Enter a number : ");
+    scanf("%d", &num);
+
+    if (num & 1)
+        MACRO(num, "Odd");
+    else
+        MACRO(num, "Even");
+
+    return 0;
+}
