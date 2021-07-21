7f0fe2eafa8fe72c5861068dc46226b5ca567f35
Sumit Kumar <sumitkn.pine@gmail.com>
2021-07-21 15:10:40 +0530

    Pointer declaration and initialization

--- pointer.c
+++ pointer.c
@@ -5,12 +5,12 @@ int main()
 	int x = 10;
 	int *ptr = &x;
 
-	printf("x = %d, &x = %d, *ptr = %d, ptr = %d\n", x, &x, *ptr, ptr);
+	printf("x = %d, &x = %p, *ptr = %d, ptr = %p\n", x, &x, *ptr, ptr);
 
 	x = 15;
-	printf("x = %d, &x = %d, *ptr = %d, ptr = %d\n", x, &x, *ptr, ptr);
+	printf("x = %d, &x = %p, *ptr = %d, ptr = %p\n", x, &x, *ptr, ptr);
 
 	*ptr = 50;
-	printf("x = %d, &x = %d, *ptr = %d, ptr = %d\n", x, &x, *ptr, ptr);
+	printf("x = %d, &x = %p, *ptr = %d, ptr = %p\n", x, &x, *ptr, ptr);
 	return 0;
 }
