12a42d7baf9831d42ecd80cfff94bbf379ed55f6
Sumit Kumar <sumitkn.pine@gmail.com>
2021-09-03 20:40:22 +0530

    printf()

---
+++ printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	char c[10] = "program";
+
+	printf("%d \n",printf("github "));
+	printf("%d \n",printf("%s ",c));
+	printf("%d \n",printf("9876 "));
+
+	return 0;
+}
