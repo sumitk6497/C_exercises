a8e5f5bb6c69dde582f6274b6930dd06a6b46dce
Sumit Kumar <sumitkn.pine@gmail.com>
2021-10-11 20:14:48 +0530

    Usage of Macro : scanf

---
+++ macro_5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define scanf "%s Git Hub \n"
+
+int main()
+{
+	printf(scanf, scanf);
+	
+	printf("%s Git Hub \n", "5\n");
+
+	return 0;
+}
+
+/*  OUTPUT
+
+%s Git Hub
+ Git Hub
+
+5
+ Git Hub
+*/
