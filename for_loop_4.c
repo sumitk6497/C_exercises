31674bbccbe2f7957c0401290350a03e8a42fced
Sumit Kumar <sumitkn.pine@gmail.com>
2021-10-28 18:30:17 +0530

    for loop : local variable

---
+++ for_loop_4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+
+	for(i = 0; i < 5 ; i++)
+	{
+		int i = 10;
+		printf("Github : %d\n", i);
+		i++;
+	}
+
+	return 0;
+}
+
+/* OUTPUT
+
+Github : 10 
+Github : 10
+Github : 10
+Github : 10
+Github : 10
+
+*/
