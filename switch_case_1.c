0ede4636badfd1d8c838693aee5ad0f9e841e6ad
Sumit Kumar <sumitkn.pine@gmail.com>
2021-10-31 18:43:16 +0530

    switch-case

---
+++ switch_case_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int i = 3;
+	switch(i)
+	{
+		case 0+1 : printf("Github\n");
+			  break;
+		case 1+2 : printf("Programming\n");
+			  break;
+		default : printf("C language\n");
+	}
+	
+	return 0;
+}
+
+/* OUTPUT
+
+Programming
+
+*/
