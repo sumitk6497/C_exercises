f0e29b7859811fda29f82a742edd5439cf9744ea
Sumit Kumar <sumitkn.pine@gmail.com>
2021-11-05 18:40:05 +0530

    new keyword in C

---
+++ new.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+int main() 
+{ 
+	int new = 5;
+
+	printf("%d\n", new); 
+
+	return 0;
+}
+
+/* OUTPUT
+
+We will get error in this program in C++ because new is a keyword in C++,so we cannot declare a variable with name new.
+But as there is no such keyword new in C, so the program is compiled and executed successfully in C.
+
+*/
