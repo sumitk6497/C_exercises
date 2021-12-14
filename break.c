caca23f1e2363193f58a7567dffe3c63d681c295
Sumit Kumar <sumitkn.pine@gmail.com>
2021-12-14 11:41:29 +0530

    break statement

---
+++ break.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	int a=10;
+
+	if(a==10)
+	{
+		printf("Hello...");
+		break;
+		printf("Ok");
+	}
+	else
+	{
+		printf("Hii");
+	}
+	printf("\n");
+
+	return 0;
+}
+
+/* ERROR
+
+A break statement can be used with looping and switch statements.
+
+*/
