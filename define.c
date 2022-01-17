2809bba9a813927f7252743f198a2d6c6c60adeb
Sumit Kumar <sumitkn.pine@gmail.com>
2022-01-17 19:19:54 +0530

    #define macro

---
+++ define.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define string char*
+
+int main()
+{
+	string myName="Sumit Kumar";
+
+	printf("My name is = %s\n", myName);
+
+	return 0;
+}
+
+/* OUTPUT
+
+My name is = Sumit Kumar
+
+string will be replaced by char *
+
+*/
