7b5f3614836eb1a3d412003366cfa7b1babb21bd
Sumit Kumar <sumitkn.pine@gmail.com>
2022-02-19 12:41:57 +0530

    typedef

---
+++ typedef_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	typedef int AAA,BBB,CCC,DDD;
+
+	AAA aaa = 10;
+	BBB bbb = 20;
+	CCC ccc = 30;
+	DDD ddd = 40;
+
+	printf("%d, %d, %d, %d\n", aaa, bbb, ccc, ddd);		//10  20  30  40
+
+	return 0;
+}
